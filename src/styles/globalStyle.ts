a3d88c1888608683daca9e653e88afe6bd77cd9b
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-04-18 13:19:07 -0300

    reenviando

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -4,8 +4,8 @@ import { DefaultTheme } from "styled-components";
 
 export const GloboStyle = createGlobalStyle<{ theme?: DefaultTheme}>`
     body, html {
-        background: ${(props) => props.theme.appBackground};
-        color: ${(props) => props.theme.appColor}; 
+        background: ${(props) => props.theme};
+        color: ${(props) => props.theme}; 
         margin: 0;
         padding: 0;
         font-family: "Inter", sans-serif;
