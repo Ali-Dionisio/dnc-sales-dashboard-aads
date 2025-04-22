19f4cd85f034e5d5f93bcdd7bb4a8995815d83c2
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-04-22 19:20:44 -0300

    com erro no login e globalStyle

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,36 @@
-import styled from "styled-components";
-
+import styled from 'styled-components'
+import { Box, Container } from '@mui/material'
+import { Grid } from '@mui/material'
 const LoginArea = styled.div`
-    background: #666;
+  background: #667;
 `
 const LoginImage = styled.div`
-    background-image: url(/login-image.svg);
-    background-size: cover;
-    height: 100vh;
-    width: 50vw;
+  background-image: url(/login-image.svg);
+  background-size: cover;
+  height: 100vh;
+  width: 50vw;
 `
 
 function Login() {
   return (
     <>
-        <LoginArea>LOGIN</LoginArea>
-        <LoginImage/>
-        
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <LoginArea>LOGIN</LoginArea>
+            </Container>
+          </Grid>
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <LoginImage />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
