df0c0c4d6c59d6a315da15d55f2a543f06fc6598
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-05-08 17:46:43 -0300

    Feat: switch theme logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { GloboStyle, lightTheme } from './styles'
+import { AppThemeProvider } from './contexts/AppThemeContext.tsx'
+import { GloboStyle } from './styles/globalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <AppThemeProvider>
       <GloboStyle />
       <App />
-    </ThemeProvider>
+    </AppThemeProvider>
   </React.StrictMode>
 )
